4a4696c9a4ea96b6fb04a515c1c81e5b96d8edb5
Trevor Scheer <trevor@apollographql.com>
2019-01-29 10:51:31 -0800

    Initial progress, flags added

--- packages/apollo/src/commands/service/check.ts
+++ packages/apollo/src/commands/service/check.ts
@@ -5,6 +5,7 @@ import { introspectionFromSchema } from "graphql";
 import { gitInfo } from "../../git";
 import { ChangeType, format, SchemaChange as Change } from "../../diff";
 import { ProjectCommand } from "../../Command";
+import { HistoricQueryParameters } from "apollo-language-server/lib/engine/operations/checkSchema";
 
 export default class ServiceCheck extends ProjectCommand {
   static aliases = ["schema:check"];
@@ -14,8 +15,27 @@ export default class ServiceCheck extends ProjectCommand {
     ...ProjectCommand.flags,
     tag: flags.string({
       char: "t",
-      description: "The published tag to check this service against",
-      default: "current"
+      description: "The published tag to check this service against"
+    }),
+    from: flags.string({
+      description:
+        "The offset in seconds from zero (x < 0) for the starting point of the requested time window",
+      default: "-86400"
+    }),
+    to: flags.string({
+      description:
+        "The offset in seconds from -0 (x < -0) for the ending point of the requested time window",
+      default: "-0"
+    }),
+    queryCountThreshold: flags.integer({
+      description:
+        "Minimum number of requests within the requested time window for a query to be considered.",
+      default: 1
+    }),
+    queryCountThresholdPercentage: flags.integer({
+      description:
+        "Number of requests within the requested time window for a query to be considered, relative to total request count. Expected values are between 0 and 0.05 (minimum 5% of total request volume)",
+      default: 0
     })
   };
 
@@ -28,16 +48,25 @@ export default class ServiceCheck extends ProjectCommand {
             if (!config.name) {
               throw new Error("No service found to link to Engine");
             }
-            const schema = await project.resolveSchema({ tag: flags.tag });
+
+            const tag = flags.tag || config.tag || "current";
+            const schema = await project.resolveSchema({ tag });
             ctx.gitContext = await gitInfo();
+
+            const historicParameters = this.validateHistoricParams({
+              from: flags.from,
+              to: flags.to,
+              queryCountThreshold: flags.queryCountThreshold,
+              queryCountThresholdPercentage: flags.queryCountThresholdPercentage
+            });
 
             ctx.checkSchemaResult = await project.engine.checkSchema({
               id: config.name,
               schema: introspectionFromSchema(schema).__schema,
               tag: flags.tag,
               gitContext: ctx.gitContext,
-              frontend: flags.frontend || config.engine!.frontend
-              // historicParameters
+              frontend: flags.frontend || config.engine.frontend,
+              historicParameters
             });
           }
         }
@@ -67,5 +96,66 @@ export default class ServiceCheck extends ProjectCommand {
       this.exit();
     }
     return;
+  }
+
+  validateHistoricParams({
+    to,
+    from,
+    queryCountThreshold,
+    queryCountThresholdPercentage
+  }: {
+    to: string;
+    from: string;
+    queryCountThreshold: number;
+    queryCountThresholdPercentage: number;
+  }): HistoricQueryParameters {
+    const toNum = Number(to);
+    const fromNum = Number(from);
+
+    if (
+      to === "0" ||
+      Number.isNaN(toNum) ||
+      toNum > 0 ||
+      !Number.isInteger(toNum)
+    ) {
+      throw new Error(
+        "Please provide a valid number for the --to flag. Valid numbers are in the range x <= -0."
+      );
+    }
+
+    if (
+      from === "0" ||
+      Number.isNaN(fromNum) ||
+      fromNum >= toNum ||
+      !Number.isInteger(fromNum)
+    ) {
+      throw new Error(
+        "Please provide a valid number for the --from flag. Valid numbers are integers in the range x < -0, and --from must be less than --to."
+      );
+    }
+
+    if (!Number.isInteger(queryCountThreshold) || queryCountThreshold < 1) {
+      throw new Error(
+        "Please provide a valid number for the --queryCountThreshold flag. Valid numbers are integers in the range x >= 1."
+      );
+    }
+
+    if (
+      queryCountThresholdPercentage < 0 ||
+      queryCountThresholdPercentage > 100
+    ) {
+      throw new Error(
+        "Please provide a valid number for the --queryCountThresholdPercentage flag. Valid numbers are in the range 0 <= x <= 100."
+      );
+    }
+
+    const asPercentage = queryCountThresholdPercentage / 100;
+
+    return {
+      to: toNum,
+      from: fromNum,
+      queryCountThreshold,
+      queryCountThresholdPercentage: asPercentage
+    };
   }
 }
